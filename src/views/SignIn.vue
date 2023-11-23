d2aff7fe09ad73ff0574a42693ae9a1746457228
miura19 <littlecloud194@gmail.com>
2023-11-23 11:44:47 +0900

    signin,home遷移

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -7,21 +7,14 @@ const router = useRouter();
 
 const email = ref('');
 const password = ref('');
-const name = ref('');
 
 const handleSubmit = async () => {
-	console.log('Name:', name.value);
 	console.log('Email:', email.value);
 	console.log('Password:', password.value);
 	try {
-		const { error, data } = await supabase.auth.signUp({
+		const { error, data } = await supabase.auth.signInWithPassword({
 			email: email.value,
 			password: password.value,
-			options: {
-				data: {
-					userName: name.value,
-				},
-			},
 		});
 
 		if (error !== null) {
@@ -30,7 +23,7 @@ const handleSubmit = async () => {
 
 		if (data) {
 			router.push({
-				name: 'MailSend',
+				name: 'Home',
 			});
 		}
 	} catch (error) {
@@ -54,14 +47,10 @@ const handleSubmit = async () => {
 		<div class="mt-40 bg-white border border-gray-300 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
 			<div class="p-4 sm:p-7">
 				<div class="text-center">
-					<h1 class="block text-2xl font-bold text-gray-800 dark:text-white">Sign Up</h1>
+					<h1 class="block text-2xl font-bold text-gray-800 dark:text-white">Sign In</h1>
 					<p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
-						Already registered?
-						<!-- <a class="text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-							href="../examples/html/signup.html">
-							Sign in here
-						</a> -->
-						<router-link to="/signin" class="text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Sign in here</router-link>
+						Not yet registered?
+						<router-link to="/" class="text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Sign up here</router-link>
 					</p>
 				</div>
 
@@ -69,17 +58,6 @@ const handleSubmit = async () => {
 					<!-- Form -->
 					<form @submit.prevent="handleSubmit">
 						<div class="grid gap-y-4">
-							<div>
-								<label for="name" class="block text-sm mb-2 dark:text-white">Your Name</label>
-								<div class="relative">
-									<input type="name" id="name" name="name" v-model="name"
-										class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm" required
-										aria-describedby="name-error">
-								</div>
-								<p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email
-									address so we can get back to you</p>
-							</div>
-							<!-- End Form Group -->
 							<!-- Form Group -->
 							<div>
 								<label for="email" class="block text-sm mb-2 dark:text-white">Email address</label>
@@ -107,7 +85,7 @@ const handleSubmit = async () => {
 							<!-- End Form Group -->
 							<button type="submit"
 								class="mt-12 w-1/2 mx-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Sign
-								up</button>
+								in</button>
 						</div>
 					</form>
 					<!-- End Form -->
